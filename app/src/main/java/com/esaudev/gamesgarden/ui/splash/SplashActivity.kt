2270611cc361e77fbeb8c3c71dc9bf8215ada7b3
Esau <yosoypawn@hotmail.com>
2022-06-14 11:33:19 -0500

    splash screen

---
+++ app/src/main/java/com/esaudev/gamesgarden/ui/splash/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.esaudev.gamesgarden.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.esaudev.gamesgarden.MainActivity
+import com.esaudev.gamesgarden.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //startActivity(Intent(this, MainActivity::class.java))
+    }
+}
