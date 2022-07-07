3c39c5e29a477a1b89aa82508db66033a3a81305
Esau <yosoypawn@hotmail.com>
2022-07-07 12:46:36 -0500

    bottom navview

--- app/src/main/java/com/esaudev/gamesgarden/MainActivity.kt
+++ app/src/main/java/com/esaudev/gamesgarden/MainActivity.kt
@@ -2,12 +2,25 @@ package com.esaudev.gamesgarden
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.inflate
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.esaudev.gamesgarden.databinding.ActivityMainBinding
+import com.esaudev.gamesgarden.databinding.ActivityMainBinding.inflate
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bnvMain.setupWithNavController(findNavController(R.id.navHostMain))
     }
 }
